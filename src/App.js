7d94eade32160e3b47b6a6174b20378e1e47f196
Brandon Newton <btnwtn@gmail.com>
2017-07-12 08:42:08 -0700

    SSR support and react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+
+const Container = styled.div`
+  max-width: 37.5em;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Home = () =>
+  <Container>
+    <h1>Home</h1>
+  </Container>;
+
+const About = () =>
+  <Container>
+    <h1>About</h1>
+  </Container>;
+
+const NoMatch = () =>
+  <Container>
+    <h1>404</h1>
+  </Container>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
     );
   }
 }
