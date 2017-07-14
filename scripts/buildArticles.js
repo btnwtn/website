0ba2928804f6604ff765304bbf7a1a0106eb5228
Brandon Newton <btnwtn@gmail.com>
2017-07-14 16:22:01 -0700

    Node 8 on netlify?

--- scripts/buildArticles.js
+++ scripts/buildArticles.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
+const { promisify } = require("util");
 
-fs.readdir("./data/articles", (err, files) => {
-  if (err) throw err;
-  console.log(JSON.stringify(files, null, 2));
-});
+const readDirAsync = promisify(fs.readdir);
+
+readDirAsync("./data/articles")
+  .then(files => {
+    console.log("Got articles:");
+    console.log(JSON.stringify(files, null, 2));
+  })
+  .catch(console.error);
